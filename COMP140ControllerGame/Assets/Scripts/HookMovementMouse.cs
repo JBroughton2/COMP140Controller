d51fbd33d03c3757c9edbd8b5ae160d9d7eb195a
Joe Broughton <jodeph.broughton@gmail.com>
2020-02-29 14:31:03 +0000

    Added basic movement using keyboard controls

---
+++ COMP140ControllerGame/Assets/Scripts/HookMovementMouse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookMovementMouse : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeedX;
+    [SerializeField]
+    private float moveSpeedY;
+
+    private void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 movementX = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movementX * Time.deltaTime * moveSpeedX;
+        Vector3 movementY = new Vector3(0f, Input.GetAxis("Vertical"), 0f);
+        transform.position += movementY * Time.deltaTime * moveSpeedY;
+    }
+
+
+}
