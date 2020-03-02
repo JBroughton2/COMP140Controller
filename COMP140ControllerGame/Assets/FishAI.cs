3c277ca8848e2b59f567bd4a492e49fc8988133c
Joe Broughton <jodeph.broughton@gmail.com>
2020-03-02 16:36:48 +0000

    Current fish moves side to side, code needs polishing
    Find a better way of doing this

--- COMP140ControllerGame/Assets/FishAI.cs
+++ COMP140ControllerGame/Assets/FishAI.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class FishAI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    [Range(0, 2)]
+    private float detectDistance;
 
-    // Update is called once per frame
-    void Update()
+    private bool movingRight = true;
+    public Transform edgeDetection;
+
+    private void Update()
     {
-        
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D edgeInfoRight = Physics2D.Raycast(edgeDetection.position, Vector2.down, detectDistance);
+        Debug.DrawRay(edgeDetection.position, Vector2.down, Color.red);
+        if(edgeInfoRight.collider == false)
+        {
+            if(movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
     }
 }
