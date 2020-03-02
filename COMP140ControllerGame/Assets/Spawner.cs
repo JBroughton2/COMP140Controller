fa7d6c41ccc9efd99f107aa984141b84450ce12b
Joe Broughton <jodeph.broughton@gmail.com>
2020-03-02 15:41:57 +0000

    Created the beginning of the Spawner Script
    Had to leave early though

---
+++ COMP140ControllerGame/Assets/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private GameObject[] fish;
+    private GameObject[] obstacles;
+    private int fishIndex;
+    private int obsacleIndex;
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
