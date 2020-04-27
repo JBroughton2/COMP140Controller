293ffe10d345b65ce6e49a7eb6830c0e4c335562
Joe Broughton <jodeph.broughton@gmail.com>
2020-04-27 13:53:35 +0100

    Added a game over screen and score to the game

---
+++ COMP140ControllerGame/Assets/Buttonmanager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttonmanager : MonoBehaviour
+{
+    public void Quitting()
+    {
+        Application.Quit();
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
--- COMP140ControllerGame/Assets/Scripts/HookManager.cs
+++ COMP140ControllerGame/Assets/Scripts/HookManager.cs
@@ -1,10 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HookManager : MonoBehaviour
 {
+    public Text scoreText;
+    public GameObject lossScreen;
+
     private bool fishHooked;
+    private bool loseGame;
+    private int score;
+
+    private void Awake()
+    {
+        Time.timeScale = 1;
+    }
+
+    private void Update()
+    {
+        scoreText.text = "Score: " + score;
+
+        if(score < 0)
+        {
+            loseGame = true;
+            Time.timeScale = 0;
+        }
+
+        if(loseGame)
+        {
+            lossScreen.SetActive(true);
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -24,6 +51,17 @@ public class HookManager : MonoBehaviour
                 fishMovement.caught = false;
                 StartCoroutine(fishMovement.ReleasedTimer());
                 child.parent = null;
+                score -= 1;
+                fishHooked = false;
+            }
+        }
+
+        if (col.CompareTag("ScoreBox") && fishHooked)
+        {
+            foreach (Transform child in transform)
+            {
+                Destroy(child.gameObject);
+                score += 1;
                 fishHooked = false;
             }
         }
